20c93d405899afeb3aeedc4814131b9d8d625fa2
knalli <knallisworld@googlemail.com>
2018-12-01 12:06:37 +0100

    Fix code style

--- dayless/measurement.go
+++ dayless/measurement.go
@@ -8,7 +8,7 @@ import (
 /**
 Convenient method for tracking function's usage time
 https://coderwall.com/p/cp5fya/measuring-execution-time-in-go
- */
+*/
 func TimeTrack(start time.Time, name string) {
 	elapsed := time.Since(start)
 	fmt.Printf("%s took %s", name, elapsed)
