ba919c730ba080a68526bbc6c9b081b4e381f85a
knalli <knallisworld@googlemail.com>
2018-12-01 12:27:54 +0100

    Solve Day 1

--- day01/main.go
+++ day01/main.go
@@ -1,7 +1,81 @@
 package main
 
-import day00Lib "de.knallisworld/aoc/aoc2018/test001/lib"
+import (
+	"de.knallisworld/aoc/aoc2018/dayless"
+	"errors"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+const AocDay = 1
+const AocDayName = "day01"
 
 func main() {
-	day00Lib.SomethingOfThis()
+	dayless.PrintDayHeader(AocDay)
+	defer dayless.TimeTrack(time.Now(), AocDayName)
+
+	fmt.Println("--- Part One ---")
+	lines, err := dayless.ReadFileToArray(AocDayName + "/puzzle1.txt")
+	if err != nil {
+		panic(err)
+		return
+	}
+	numbers, err := transformStringsToNumbers(lines)
+	if err != nil {
+		panic(err)
+		return
+	}
+	fmt.Printf("The resulting frequency is: %d\n", sumNumbers(numbers))
+	fmt.Println()
+
+	fmt.Println("--- Part Two ---")
+	interval, index, result := findDuplicateResults(numbers)
+	fmt.Printf("The first frequency reached is: %d (interval=%d, index=%d)\n", result, interval, index)
+	fmt.Println()
+}
+
+func transformStringsToNumbers(lines []string) ([]int, error) {
+	var numbers []int
+	for _, line := range lines {
+		i, err := strconv.Atoi(line[1:])
+		if err != nil {
+			return nil, err
+		}
+		if line[0] == '+' {
+			numbers = append(numbers, i)
+		} else if line[0] == '-' {
+			numbers = append(numbers, -i)
+		} else {
+			return nil, errors.New("invalid sign found")
+		}
+	}
+	return numbers, nil
+}
+
+func sumNumbers(numbers []int) int {
+	result := 0
+	for _, i := range numbers {
+		result += i
+	}
+	return result
+}
+
+func findDuplicateResults(numbers []int) (interval int, index int, result int) {
+
+	var counts = make(map[int]int)
+
+	result = 0
+	counts[result] = 1
+
+	for {
+		interval++
+		for index, i := range numbers {
+			result += i
+			counts[result] = counts[result] + 1
+			if counts[result] > 1 {
+				return interval, index, result
+			}
+		}
+	}
 }
