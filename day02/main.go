0fce833f74b1dd9ed51d472a0ab0430c3ac796bc
knalli <knallisworld@googlemail.com>
2018-12-02 12:53:36 +0100

    Solve Day2 (Part Two)

--- day02/main.go
+++ day02/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"de.knallisworld/aoc/aoc2018/dayless"
+	"errors"
 	"fmt"
 	"log"
+	"math"
 	"time"
 )
 
 const AocDay = 2
 const AocDayName = "day02"
-const debug = false
+const debug = true
+const trace = false
 
 func main() {
 	dayless.PrintDayHeader(AocDay)
@@ -20,10 +23,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("The checksum is: %d", checksum(lines))
+	fmt.Printf("The checksum is: %d\n", checksum(lines))
 	fmt.Println()
 
 	dayless.PrintStepHeader(2)
+	fmt.Printf("This letters are common: %s\n", findBestCommonLinePart(lines))
 	fmt.Println()
 }
 
@@ -39,7 +43,7 @@ func countCharactersDuplications(lines []string) map[int]int {
 		for i := 2; i < len(line); i++ {
 			if containsValue(chars, i) {
 				counts[i]++
-				if debug {
+				if trace {
 					log.Printf("Line '%s' contains IDs exactly %d times", line, i)
 				}
 			}
@@ -64,4 +68,71 @@ func containsValue(data map[int32]int, search int) bool {
 		}
 	}
 	return false
+}
+
+func findBestCommonLinePart(lines []string) string {
+
+	var bestLine1 string
+	var bestLine2 string
+	var bestDiff = math.MaxInt16
+
+	for _, line1 := range lines {
+		for _, line2 := range lines {
+			if line1 == line2 {
+				continue
+			}
+			diff := diffString(line1, line2)
+			if diff < bestDiff {
+				bestDiff = diff
+				bestLine1 = line1
+				bestLine2 = line2
+			}
+			if trace {
+				fmt.Printf("Line '%s' and '%s' differ by: %d\n", line1, line2, diff)
+			}
+		}
+	}
+
+	if debug {
+		fmt.Printf("BEST MATCH: Line '%s' and '%s' differ by: %d\n", bestLine1, bestLine2, bestDiff)
+	}
+
+	return extractCommonParts(bestLine1, bestLine2)
+}
+
+/**
+Returns the number of different characters
+*/
+func diffString(line1, line2 string) int {
+
+	// ensure first line is the smaller one
+	if len(line1) > len(line2) {
+		return diffString(line2, line1)
+	}
+
+	var result int
+	for i := range line1 {
+		if line1[i] != line2[i] {
+			result++
+		}
+	}
+	result += len(line2) - len(line1) // add the rest if line2 is actually longer
+
+	return result
+}
+
+func extractCommonParts(line1 string, line2 string) string {
+
+	if len(line1) != len(line2) {
+		panic(errors.New("the length of both strings are not equal (not supported)"))
+	}
+
+	result := ""
+	for i := range line1 {
+		if line1[i] == line2[i] {
+			result += string(line1[i])
+		}
+	}
+
+	return result
 }
