fd92abbc1735bb3e8b03984ff60aaa071cd8a07f
knalli <knallisworld@googlemail.com>
2018-12-12 09:08:15 +0100

    Enhance styling

--- dayless/measurement.go
+++ dayless/measurement.go
@@ -11,5 +11,5 @@ https://coderwall.com/p/cp5fya/measuring-execution-time-in-go
 */
 func TimeTrack(start time.Time, name string) {
 	elapsed := time.Since(start)
-	fmt.Printf("%s took %s\n", name, elapsed)
+	fmt.Printf("⏱ %s took %s\n", name, elapsed)
 }
