cb03eda1bc6808ff1326e1a0ddb82668c27d15e3
knalli <knallisworld@googlemail.com>
2018-12-18 18:32:43 +0100

    Add base ParsInt (same as atoi but w/o err)

--- dayless/functions.go
+++ dayless/functions.go
@@ -1,5 +1,15 @@
 package dayless
 
+import "strconv"
+
 func Greet(name string) {
 	println("Hello " + name)
+}
+
+func ParseInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
 }
